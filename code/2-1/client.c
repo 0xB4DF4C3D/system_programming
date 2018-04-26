4c18ca614afbc57e7bcc54b02a13422dd7687d80
0xDEADBEEF <dongho971220@gmail.com>
2018-04-26 22:49:54 +0900

    style(client): Add comment

--- code/2-1/client.c
+++ code/2-1/client.c
@@ -9,6 +9,7 @@
 
 #include <arpa/inet.h> // for socket programming
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,43 +25,53 @@ typedef enum {
 } Proxy_constants;
 
 int main(){
+    // buffer for URL inputted by user or result from server
     char buf[BUFSIZ] = {0};
 
-    char haddr[] = "127.0.0.1";
-
+    // struct for server address
     struct sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(server_addr));
 
+    // variables for socket
     int socket_fd        = 0;
     socklen_t len_socket = 0;
 
+    // try open a stream socket
     if((socket_fd = socket(PF_INET, SOCK_STREAM, 0)) < 0){
         fprintf(stderr, "[!](client) can't open stream socket\n");    
         exit(EXIT_FAILURE);
     }
 
+	// initialize server_addr
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(haddr);
+    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
     server_addr.sin_port = htons(PROXY_PORTNO);
 
+	// connect socket_fd to server_addr
     if(connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
         fprintf(stderr, "[!](client) can't connect\n");
         exit(EXIT_FAILURE);
     }
 
-    printf("input url > ");
-    while((len_socket = read(STDIN_FILENO, buf, sizeof(buf))) > 0){
+    // interact with the server 
+    while(true){
+        // input url by the user without a linebreak
+        printf("input url > "); fflush(stdout);
+        len_socket = read(STDIN_FILENO, buf, sizeof(buf));
         buf[strlen(buf)-1] = '\0';
 
+        // if the url is 'bye' then break
         if(!strncmp(buf, "bye", 3)) break;
 
+        // send the url to the server
         if(write(socket_fd, buf, strlen(buf)) > 0){
+            // receive the result from the server
             if((len_socket = read(socket_fd, buf, sizeof(buf))) > 0){
-                printf("%s\n", buf);
+                write(STDOUT_FILENO, buf, len_socket);
+                write(STDOUT_FILENO, "\n", 1);
                 memset(&buf, 0, sizeof(buf));
             }
         }
-        printf("input url > ");
     }
 
     close(socket_fd);
